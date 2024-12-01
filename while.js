666950ff7103a51060cd2ad0ec55c4862252db38
VIN124-VINNI <vineelareddy925@gmail.com>
2024-12-01 22:49:37 +0530

    Add files via upload

---
+++ while.js
@@ -0,0 +1,34 @@
+// 1.Use switch case and take Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday as cases and any expression related to case 
+switch ("friday"){
+    case ("monday"):
+        console.log("today is monday")
+        break
+        case ("tuesday"):
+        console.log("today is tuesday")
+        break
+        case ("wednesday"):
+        console.log("today is wednesday")
+        break
+        case ("thursday"):
+        console.log("today is thursday")
+        break
+        case ("friday"):
+        console.log("today is friday")
+        break
+        case ("saturday"):
+        console.log("today is saturday")
+        break
+        case ("sunday"):
+        console.log("today is sunday")
+        default:
+            console.log("not matched")
+}
+// For of loop:-- iterate array of elements having number 1  to 5
+// 3. For of loop:-- take string Nd iterate characters
+var a=[1,2,3,4,5]
+for(var num of a)
+    console.log(num)
+var b="vinni"
+for(var char of b)
+    console.log(char)
+
